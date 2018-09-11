c2382dae2ef7d78db4de080fcf6cd6cdf5ba2bf4
Rebpat21 <42726414+Rebpat21@users.noreply.github.com>
2018-09-10 19:05:34 -0500

    HW01

---
+++ HW01.hs
@@ -0,0 +1,26 @@
+import Data.List
+import System.IO
+
+module HW01  -- in file nameed HW01.hs
+   ( prodSqSmall, xor, implies, ccArea, addTax, subTax,
+     validDay, roman )
+where
+
+prodSqSmall :: Double -> Double -> Double -> Double
+-- doubleList = [2.0, 4.0, 3.0]
+-- min2 = 2 : doubleList
+-- prodSqSmall =
+
+
+xor :: Bool -> Bool -> Bool
+
+implies :: Bool -> Bool -> Bool
+
+ccArea :: Double -> Double -> Double
+
+addTax :: Double -> Double -> Double
+subTax :: Double -> Double -> Double
+
+validDay :: (Int, Int, Int) -> Bool
+
+roman :: Int -> String
